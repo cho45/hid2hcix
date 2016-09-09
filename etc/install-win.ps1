80e1baa513beba51b217060ed424ef9530ca9cc6
cho45 <cho45@lowreal.net>
2016-09-10 01:11:25 +0900

    a

--- etc/install-win.ps1
+++ etc/install-win.ps1
@@ -1,8 +1,7 @@
+$SystemProperty = $(Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders')
+$dest = $SystemProperty.'Common Startup' + "\hid2hcix.exe"
 
-Unregister-ScheduledJob -Name HID2HCI
-
-Invoke-WebRequest -Uri "https://github.com/cho45/hid2hcix/releases/download/v1.0.0/hid2hcix.exe" -OutFile "C:\hid2hcix.exe"
-
-$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-Register-ScheduledJob -Trigger $trigger -FilePath C:\hid2hcix.exe -Name HID2HCI -Credential (Get-Credential)
-
+# $UserProperty = $(Get-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders')
+# $dest = $UserProperty.Startup
+echo $dest
+Invoke-WebRequest -Uri "https://github.com/cho45/hid2hcix/releases/download/v1.0.0/hid2hcix.exe" -OutFile $dest
